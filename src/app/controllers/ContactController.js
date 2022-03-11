49a20afb71cb13bd1f236263d38f44746127734e
anniasebold <anniasebold3.0@gmail.com>
2022-03-11 17:48:48 -0300

    creating put route and controller

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -27,15 +27,13 @@ class ContactController {
     } = request.body;
 
     const contactExists = await ContactsRepository.findByEmail(email);
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email is already in use' });
+    }
 
     if (!name) {
       return response.status(400).json({ error: 'Name is required' });
     }
-
-    if (contactExists) {
-      return response.status(400).json({ error: 'This email is already been taken' });
-    }
-
     const contact = await ContactsRepository.create({
       name, email, phone, category_id,
     });
@@ -43,8 +41,33 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(400).json({ error: 'Contact not fund' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This email is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.send(contact);
   }
 
   async delete(request, response) {
