de03ca9dd2c60b9dbc636d488869620d296ddb38
anniasebold <anniasebold3.0@gmail.com>
2022-03-21 00:49:52 -0300

    inject category name in select of contacts

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -9,7 +9,11 @@ class ContactsRepository {
       Se não for enviado nada nos queryParams então o valor default vai ser 'ASC'
     */
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    const rows = await db.query(`
+    SELECT contacts.*, categories.name AS category_name
+    FROM contacts
+    LEFT JOIN categories ON categories.id = contacts.category_id
+    ORDER BY contacts.name ${direction}`);
     return rows;
   }
 
